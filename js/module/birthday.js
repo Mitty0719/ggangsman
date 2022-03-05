b0c80ac3a04fc836bad390ffc6ff899c40db6df1
Mitty0719 <sprout071900@gmail.com>
2022-03-05 21:15:23 +0900

    Add progress click event

--- js/module/birthday.js
+++ js/module/birthday.js
@@ -5,12 +5,18 @@ export class Birthday{
     this.playButtonImg = document.querySelector('.controller-button-con .button-play .play-img');
     this.progressCon = document.querySelector('.controller-progress-con');
     this.progress = document.querySelector('.controller-progress-con .current-progress');
+    this.currentTime = document.querySelector('.controller-time-con .current-time');
+    this.wholeTime = document.querySelector('.controller-time-con .whole-time');
 
     this.playButton.addEventListener('click', this.clickPlayButton.bind(this));
     this.assets = ['./src/images/controller_pause.png', './src/images/controller_play.png'];
     this.isPlaying = false;
 
-    
+    setTimeout(()=>{
+      this.wholeTime.innerHTML = `${Math.floor(this.music.duration / 60).toString().padStart(2, '0')}:${Math.floor(this.music.duration % 60).toString().padStart(2, '0')}`;
+    }, 100);
+
+    this.progressCon.addEventListener('click', this.setCurrentTime.bind(this));
   }
   clickPlayButton(){
     if(this.isPlaying){
@@ -30,7 +36,16 @@ export class Birthday{
     this.isPlaying = false;
   }
   updateProgress(){
-    this.progress.style.width = `${this.music.currentTime / this.music.duration * 100}%`;
-    console.log(this.music.currentTime, this.music.duration);
+    this.currentTime.innerHTML = `${Math.floor(this.music.currentTime / 60).toString().padStart(2, '0')}:${Math.floor(this.music.currentTime % 60).toString().padStart(2, '0')}`;
+    this.progress.style.width = `${Math.floor(this.music.currentTime / this.music.duration * 100)}%`;
+
+    if(this.music.currentTime === this.music.duration){
+      this.music.currentTime = 0;
+      this.pauseMusic();
+    }
+  }
+  setCurrentTime(e){
+    const ratio = e.offsetX / e.target.clientWidth;
+    this.music.currentTime = Math.floor(this.music.duration * ratio);
   }
 }
